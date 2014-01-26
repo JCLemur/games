5687f3a9a6fbe161ec2258d26c96e326ec1ec4b9
Jochen Kupperschmidt <homework@nwsnet.de>
2014-01-26 06:31:00 +0100

    Added Fizz Buzz implementation in Python.

---
+++ fizzbuzz/README.rst
@@ -0,0 +1,7 @@
+Fizz Buzz
+=========
+
+Implementations of the `Fizz Buzz`_ game in multiple programming
+languages.
+
+.. _Fizz Buzz: http://en.wikipedia.org/wiki/Fizzbuzz
---
+++ fizzbuzz/fizzbuzz.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Fizz Buzz
+=========
+
+A functional programming-orientated implementation of the `Fizz Buzz`_
+game.
+
+.. _Fizz Buzz: http://en.wikipedia.org/wiki/Fizzbuzz
+
+:Copyright: 2014 `Jochen Kupperschmidt <http://homework.nwsnet.de/>`_
+:Date: 26-Jan-2014
+:License: MIT
+"""
+
+from functools import partial
+try:
+    from future_builtins import map
+except ImportError:
+    pass  # Python 3
+from itertools import count, islice
+import unittest
+
+
+divisible_by = lambda dividend, divisor: dividend % divisor == 0
+divisible_by_3 = partial(divisible_by, divisor=3)
+divisible_by_5 = partial(divisible_by, divisor=5)
+divisible_by_3_and_5 = lambda x: divisible_by_3(x) and divisible_by_5(x)
+
+REPLACEMENTS = [
+    (divisible_by_3_and_5, 'Fizz Buzz'),
+    (divisible_by_3, 'Fizz'),
+    (divisible_by_5, 'Buzz'),
+]
+
+def replace(x):
+    """Return the replacement defined for the given number, or the
+    number itself.
+    """
+    for predicate, replacement in REPLACEMENTS:
+        if predicate(x):
+            return replacement
+    return str(x)
+
+def fizzbuzz(limit):
+    """Generate the Fizz Buzz sequence from 1 until the given limit
+    (inclusive).
+    """
+    numbers = islice(count(1), limit)
+    return map(replace, numbers)
+
+
+class FizzBuzzTestCase(unittest.TestCase):
+
+    def test(self):
+        expected = [
+            '1', '2', 'Fizz', '4', 'Buzz',
+            'Fizz', '7', '8', 'Fizz', 'Buzz',
+            '11', 'Fizz', '13', '14', 'Fizz Buzz',
+            '16', '17', 'Fizz', '19', 'Buzz',
+            'Fizz', '22', '23', 'Fizz', 'Buzz',
+            '26', 'Fizz', '28', '29', 'Fizz Buzz',
+            '31', '32', 'Fizz', '34', 'Buzz',
+            'Fizz', '37', '38', 'Fizz', 'Buzz',
+        ]
+        actual = list(fizzbuzz(40))
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
